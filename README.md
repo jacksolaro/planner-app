90309a9e53d432d0181dd0c0bccf241e6d46a4fc
jacksolaro <jacksolaro@gmail.com>
2020-09-18 17:49:03 -0700

    added demo image and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# planner-app
+# Planner App
+
+![GIF demo of trivia app that cycles through welcome page, game play, and leaderboard]()
+
+
+**GitHub Repo:** https://github.com/jacksolaro/trivia
+
+**GitHub Pages Link:** https://jacksolaro.github.io/trivia/
+
+## About the project
+This is an app that allows you to plan out your work day! The window is broken out into hour blocks from 9AM to 5PM with an area to input your tasks for each hour.
+
+### Languages Used:
+- Javascript (jQuery)
+
+## How to Use
+The Planner App works as follows:
+1. Select the empty space to the right of the hour to input a To do for that block. The block shading code is as follows:
+    - Gray: Past Hour
+    - Red: Current Hour
+    - Green: Future Hour
+2. After you have inputted your To Do, you must hit the blue save button to save that to do. 
+3. To Do's are saved to local storage, so you can close the page or refresh and your to do will remain
+
+## Future Scope:
+- Add option to add tasks for future dates
+- Automatically save tasks without clicking the save button
+- Add option to add DISTINCTIVE tasks within each hour
+- Add task status option
+
+## Contributors
+@jacksolaro - Javascript
+
+## Contact
+If you want to contact me you can reach me at jacksolaro@gmail.com.
--- script.js
+++ script.js
@@ -4,18 +4,19 @@
 var timeblockContainer = $(".container");
 
 
+
 // ==================================================
 // Variables
 // ==================================================
 
+// Grab the current hour (in military time) using moment.js
 var currMilitaryHour = moment().format("H");
-console.log(currMilitaryHour);
 
 // Show time on top of calendar using moment. Set the text of the p tag to be the current time 
 var currentDayEl = $("#currentDay")
 currentDayEl.text(moment().format("dddd, MMMM Do"));
 
-// TODO: Create an array to hold the holes that I need (9AM - 5PM)
+// Create an array to hold the Work Hours and Content of the Planner
 var plannerContent = [
     {
         "time": "9AM",
@@ -55,11 +56,16 @@ var plannerContent = [
     }
 ];
 
+// If there is data from local storage, grab that
+var storedPlannerContent = JSON.parse(localStorage.getItem("plannerContent")) || plannerContent;
+
+
+
 // ==================================================
 // Page Load
 // ==================================================
-storedPlannerContent = JSON.parse(localStorage.getItem("plannerContent")) || plannerContent;
-console.log(storedPlannerContent)
+
+// Render the planner
 renderPlanner()
 
 
@@ -67,15 +73,15 @@ renderPlanner()
 // Functions
 // ==================================================
 
-function renderPlanner(){
-    // for loop to go through the array of times to create blocks for each
+function renderPlanner() {
+    // for loop to go through the storedPlannerContent and create planner rows for each time block
     for (let index = 0; index < storedPlannerContent.length; index++) {
-    
-        // TODO: create elements for timeblocks and assign classes. CompareTime computes the time on the row against the current time and returns the class of past, present, or future to give formatting to each of the rows
+
+        // create elements for timeblocks, assign classes, and fill with data from storedPlannerContent. Call compareTime() to add class for Past, Present, Future
         var newTimeblock = $("<div>", {
             "class": `row time-block ${compareTime(index)}`,
             "IndexVal": index
-            })
+        })
         var newHourCol = $("<div>", {
             "class": 'col-md-1 hour',
             "text": storedPlannerContent[index].time,
@@ -86,80 +92,52 @@ function renderPlanner(){
         })
         var newSaveBtn = $("<button>", {
             "class": 'col-md-1 saveBtn',
-            "text": "SAVE" 
+            "text": "SAVE"
         })
-    
+
         // Append new elements to the new time block
-        newTimeblock.append(newHourCol,newTextArea,newSaveBtn);
-    
+        newTimeblock.append(newHourCol, newTextArea, newSaveBtn);
+
         // Append the new time block to the time block container
         timeblockContainer.append(newTimeblock);
     }
 }
 
-// Compare current time on row to current time (hint: could grab in military time and then compare to index+9)
+// Returns Past, Present, or Future to be added to newTimeBlocks for styling. Compares the index+9 to the current military hour to determine if past, present, or future
 function compareTime(index) {
-    switch(true) {
-        case (parseInt(index+9) > parseInt(currMilitaryHour)):
+    switch (true) {
+        case (parseInt(index + 9) > parseInt(currMilitaryHour)):
             // FUTURE
             return "future";
-        case (parseInt(index+9) < parseInt(currMilitaryHour)):
+        case (parseInt(index + 9) < parseInt(currMilitaryHour)):
             // PAST
             return "past";
-        case (parseInt(index+9) === parseInt(currMilitaryHour)):
+        case (parseInt(index + 9) === parseInt(currMilitaryHour)):
             // PRESENT
             return "present";
         default:
             break;
     }
-
 }
-
-// save / load 
-// use "this" to figure out which row was clicked
 
 
 // ==================================================
 // Click Events
 // ==================================================
 
-// TODO: add click event to for button
-$(document).on("click",".time-block", function(e) {
-    if(e.target.matches("button")){
-        // console.log("click on button")
-        // console.log($(this));
-        // Grab the value of the text area and save to a var (need to be able to save the text from the text area that is in the same row as button)
+// Click Event for Save Button
+$(document).on("click", ".time-block", function (e) {
+    if (e.target.matches("button")) {
+        // Grab the value of the text area and save to a var
         var rowContent = ($(this)[0].children[1].value);
-        // console.log(rowContent);
+
+        // Grab the value of the row that is being saved
         var rowIndex = ($(this)[0].attributes[1].value);
-        // console.log(rowIndex);
 
+        // Update the storedPlannerContent variable
         storedPlannerContent[rowIndex].content = rowContent;
-        // console.log(plannerContent);
 
+        // Update Local Storage
         localStorage.setItem("plannerContent", JSON.stringify(storedPlannerContent))
     }
 })
-
-
-
-// TODO: save the information for that row to local storage (localStorage.setItem)
-// localStorage.setItem("test1","first item")
-// localStorage.setItem("test2","second item")
-
-// var test = localStorage.getItem("test1")
-
-
-// ==================================================
-// TODO LIST
-// ==================================================
-
-// TODO: Create One Row with time, text area, and button using jQuery
-// Here is the layout of what we need to make using jQuery
-{/* <div class="row time-block">
-<div class="col-md-1 hour">
-  9AM
-</div>
-<textarea class="col-md-10"></textarea>
-<button class="col-md-1 saveBtn">Save</button>
-</div> */}
